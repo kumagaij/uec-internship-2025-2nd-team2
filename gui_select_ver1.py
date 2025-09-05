740a08e996989269ba5a25140806c3efb3319470
Sakurai333333 <s2312086@gl.cc.uec.ac.jp>
2025-09-05 17:08:15 +0900

    Add files via upload

---
+++ gui_select_ver1.py
@@ -0,0 +1,25 @@
+import tkinter
+import tkinter.ttk
+
+# def select_var(e):
+#     mes["text"] = combo1.get()
+
+root = tkinter.Tk()
+root.title("conbobox")
+root.geometry("400x200")
+la01 = tkinter.Label(root, text="フィルタレベル",font=("System",12))
+la01.place(x=20,y=50)
+
+#設定
+data_list1 = ["3","5","7"]
+combo1 = tkinter.ttk.Combobox(state = "readonly",
+values = data_list1,font = ("System",12))
+
+combo1.set(data_list1[0])
+combo1.bind("<<ComboboxSelected>>")
+combo1.place(x=150,y=50)
+
+# mes = tkinter.Message(root,text="ok",
+#                       width=50,bg="white",font=("System",12))
+# mes.place(x=350,y=46)
+root.mainloop()
